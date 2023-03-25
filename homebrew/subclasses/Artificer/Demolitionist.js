eb54369340f5c2a57ea6fa812c4f2896d82a221c
Exiate <56456080+Exiate@users.noreply.github.com>
2023-03-25 17:37:42 -0400

    1

--- homebrew/subclasses/Artificer/Demolitionist.js
+++ homebrew/subclasses/Artificer/Demolitionist.js
@@ -101,7 +101,7 @@ AddSubClass("artificer", "Demolitionist", {
 				"Siege Damage: Damage dealt to objects and structures is doubled by effects and attacks which deal Siege damage."
 			]),
 			calcChanges : {
-				atkAdd : ["if (classes.known.artificer && classes.known.artificer.level > 4 && ((/basic/i).test(fields.Description) || (/basic/i).test(WeaponName))) {fields.Description += (fields.Description ? '; ' : '') + levels.map(function (n) {return (n < 11 ? 1 : n < 17 ? 2 : 3) + 'd4 damage'})};"]
+				atkAdd : ["if (classes.known.artificer && classes.known.artificer.level > 4 && ((/basic/i).test(fields.Description) || (/basic/i).test(WeaponName))) {fields.Description += (fields.Description ? '; ' : '') + levels.map(n < 11 ? 1 : n < 17 ? 2 : 3) + 'd4 damage'};"]
 			}
 		},
 		//"subclassfeature9" : {
